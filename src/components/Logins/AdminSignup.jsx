c6a2874c1ecd0b47d8c2a5d176a0b71f655505ac
mumbi <mumbi099@gmail.com>
2024-05-28 13:10:29 +0300

    installed axios and react-router dom, plus edis on signup page

--- src/components/Logins/AdminSignup.jsx
+++ src/components/Logins/AdminSignup.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+//import bkg from '../../images/bkg.jpg'
+import './logins.css'
 
 function AdminSignup() {
+
+    // const [formData, setFormData] = useState({
+    //     email: "",
+    //     password: "",
+    //   });
+    //   const [errorMsg, setErrMsg] = useState("");
+    //   const [successMsg, setSuccessMsg] = useState("");
+    
+    //   const handleChange = (e) => {
+    //     setFormData({ ...formData, [e.target.name]: e.target.value });
+    //   };
+    
+    //   const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     try {
+    //       const response = await axios.post("/login", formData);
+    //       if (response.status === 200) {
+    //         const data = response?.data;
+    //         setSuccessMsg("Login successful!");
+    //         localStorage.setItem("userID", data.id);
+    //       } else {
+    //         setErrMsg("An error occurred while logging in");
+    //         setTimeout(() => {
+    //           setErrMsg("");
+    //         }, 3000);
+    //       }
+    //     } catch (error) {
+    //       console.error(error);
+    //       setErrMsg("An error occurred while logging in");
+    //       setTimeout(() => {
+    //         setErrMsg("");
+    //       }, 3000);
+    //     }
+    //   };
+
   return (
-    <div className='auth-container'>
-        
+    <div className='auth-container bg-gray-bkg h-screen flex justify-center items-center'>
+        <div className='auth-wrapper m-[0] flex w-[900px]'>
+            <div className="login-bkg w-[50%]">
+                <h3 className='font-playfair text-4xl font-bold text-white text-center py-[65%]'>Welcome Back</h3>
+            </div>
+            <div className='form-content w-[50%] bg-white'>
+                <h5 className="text-4xl font-medium mt-[20px] mb-[10px] text-center text-black"> Log In </h5>
+                <p className="text-center text-black mb-[10px]">Not a member? </p>
+
+                <div className="form">
+
+                </div>
+            </div>
+
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,12 @@ module.exports = {
         'playfair': ['Playfair Display', 'serif'],
       },
       colors: {
-       
+        'text-color': '#fff',
+        'main-blue': '#032234',
+        'gray-bkg': '#e6f7ff',
+        'other-blue': '#0d4e78',
+        'hover-blue': '#256086',
+        
       },
       boxShadow: {
         'blue': '0 0 10px rgba(142, 219, 245, 0.966)',
