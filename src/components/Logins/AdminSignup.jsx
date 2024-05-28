9cb31485ada0981f1a0f249b9a4db71b06755988
mumbi <mumbi099@gmail.com>
2024-05-28 15:54:13 +0300

    Update on signup

--- src/components/Logins/AdminSignup.jsx
+++ src/components/Logins/AdminSignup.jsx
@@ -1,58 +1,45 @@
 import React from 'react'
 //import bkg from '../../images/bkg.jpg'
 import './logins.css'
+// import {Link} from react-router-dom;
 
 function AdminSignup() {
 
-    // const [formData, setFormData] = useState({
-    //     email: "",
-    //     password: "",
-    //   });
-    //   const [errorMsg, setErrMsg] = useState("");
-    //   const [successMsg, setSuccessMsg] = useState("");
-    
-    //   const handleChange = (e) => {
-    //     setFormData({ ...formData, [e.target.name]: e.target.value });
-    //   };
-    
-    //   const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //       const response = await axios.post("/login", formData);
-    //       if (response.status === 200) {
-    //         const data = response?.data;
-    //         setSuccessMsg("Login successful!");
-    //         localStorage.setItem("userID", data.id);
-    //       } else {
-    //         setErrMsg("An error occurred while logging in");
-    //         setTimeout(() => {
-    //           setErrMsg("");
-    //         }, 3000);
-    //       }
-    //     } catch (error) {
-    //       console.error(error);
-    //       setErrMsg("An error occurred while logging in");
-    //       setTimeout(() => {
-    //         setErrMsg("");
-    //       }, 3000);
-    //     }
-    //   };
-
   return (
     <div className='auth-container bg-gray-bkg h-screen flex justify-center items-center'>
-        <div className='auth-wrapper m-[0] flex w-[900px]'>
+        <div className='auth-wrapper ml-[100px] flex w-[1000px]'>
             <div className="login-bkg w-[50%]">
-                <h3 className='font-playfair text-4xl font-bold text-white text-center py-[65%]'>Welcome Back</h3>
+                <h3 className='font-playfair text-6xl font-bold text-white text-center py-[55%]'>Welcome <br /> Back</h3>
             </div>
-            <div className='form-content w-[50%] bg-white'>
-                <h5 className="text-4xl font-medium mt-[20px] mb-[10px] text-center text-black"> Log In </h5>
-                <p className="text-center text-black mb-[10px]">Not a member? </p>
+            <div className='form-content bg-white w-[50%]  px-[3%]'>
+                <h5 className="text-4xl font-bold mt-[60px] mb-[20px] text-black"> Log In </h5>
+                <p className="text-black mb-[50px]">Not a member? <span>Sign up</span> instead</p>
 
                 <div className="form">
-
+                    <form action="">
+                        <div className="mb2 mb-[20px]">
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Email </label>
+                            <input
+                                placeholder="email@example.com" type="email" id="email" name="email"
+                                className="mt-3 p-2 w-full rounded-md bg-gray-bkg focus:outline-none" required 
+                            />
+                        </div>
+                        <div className="mb2 mb-[20px]">
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Password </label>
+                            <input
+                                placeholder="Enter password" type="password" id="password" name="password"
+                                className="mt-3 p-2 w-full rounded-md bg-gray-bkg focus:outline-none" required 
+                            />
+                        </div>
+                    </form>
+                    <div className="auth-button flex justify-center items-center">
+                        <button className='create-account uppercase bg-other-blue text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 w-full mt-7'>
+                        Login
+                        </button>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
   )
