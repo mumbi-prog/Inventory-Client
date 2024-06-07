e7e754376f125177d0f6bb160c0f304dae744761
mumbi <mumbi099@gmail.com>
2024-06-07 10:57:07 +0300

    Updated the AllRoutes

--- src/components/Routes/AllRoutes.jsx
+++ src/components/Routes/AllRoutes.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import HomePage from '../Pages./Home';
 import AdminLogin from "../Logins/AdminLogin";
 import AdminSignup from "../Logins/AdminSignup";
+import Dashboard from "../Admin/Dashboard";
+import CreateProduct from "../Admin/CreateProduct";
+import UserList from "../Admin/UserList";
+import WelcomeAdmin from "../Admin/WelcomeAdmin";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<dminLogin/>} />
       <Route path="/adminsignup" element={<AdminSignup />} />
       <Route path="/adminlogin" element={<AdminLogin/>} />
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/newproduct" element={<CreateProduct />}/>
+      <Route path="/userlist" element={<UserList />}/>
+      <Route path="/welcomeadmin" element={<WelcomeAdmin />} />
     </Routes>
   );
 };
