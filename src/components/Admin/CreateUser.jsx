cd11ac0d592df08b3394915f4066a0f6bdbfd824
mumbi <mumbi099@gmail.com>
2024-06-12 16:14:36 +0300

    imported api and added useState hook

---
+++ src/components/Admin/CreateUser.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'; 
+import api from './../Api/api'; 
+
+function CreateUser() {
+    
+
+
+
+    return (
+        <div>CreateUser
+           
+        </div>
+    );
+}
+
+export default CreateUser;
