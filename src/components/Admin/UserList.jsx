78057bd0622f2a7810efffca09d0a3398ddbcc23
mumbi <mumbi099@gmail.com>
2024-06-13 10:56:15 +0300

    Created UserList to handle http ge users request

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../Api/api.jsx'
 
 function UserList() {
+    const [userDetails, setUserDetails] = useState([]);
+    
+    useEffect(() => {
+        const fetchUserList = async () =>{
+            try{
+                const response = await api.get("http://localhost:3000/users");
+                if (response.status === 200 ){
+                    console.log("All users fetched successfully!!")
+                } else {
+                    console.log("Error fetching users", response.status)
+                }
+            }
+            catch(error){
+                console.error(error)
+            }
+
+        };
+        fetchUserList();
+    }, [])
   return (
     <div>UserList</div>
   )
