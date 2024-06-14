de2fb11c39ad2a56c032e9dafcca9a94b78791b2
mumbi <mumbi099@gmail.com>
2024-06-14 12:35:06 +0300

    Updated userlist table styling

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -27,24 +27,24 @@ function UserList() {
   return (
     <div className='sect-container mt-[20px]'>
         <h1 className='comp-title text-hover-blue font-bold capitalize text-3xl mb-[15px]'>All Users</h1>
-        <div className="users-table-container">
+        <div className="users-table-container rounded-md bg-gray-100 py-[20px] px-[50px] inline-block mt-[25px]">
             <div className="users-table">
                 <table>
-                    <thead>
+                    <thead className='bg-hover-blue'>
                         <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Department</th>
+                            <th className="w-[200px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">First Name</th>
+                            <th className="w-[200px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Last Name</th>
+                            <th className="w-[300px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Email</th>
+                            <th className="w-[200px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px]">Department</th>
                         </tr>
                     </thead>
                     <tbody>
                         {userDetails.map((user, index) => (
-                           <tr key={index}>
-                                <td>{user.first_name}</td>
-                                <td>{user.last_name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.department}</td>
+                           <tr key={index} className='border-l-4 border-r-4 border-transparent hover:border-blue-500 p-[20px] text-center'>
+                                <td className='py-[10px] px-[10px]'>{user.first_name}</td>
+                                <td className='py-[10px] px-[10px]'>{user.last_name}</td>
+                                <td className='py-[10px] px-[10px]'>{user.email}</td>
+                                <td className='py-[10px] px-[10px]'>{user.department}</td>
                            </tr>
 
                         ))}
