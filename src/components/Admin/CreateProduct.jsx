521ead3eddfabcafd054f53b06f890336c3a4683
Sylvia Mumbi <mumbi099@gmail.com>
2024-06-15 04:35:40 +0300

    Updated Create Product component and styling

--- src/components/Admin/CreateProduct.jsx
+++ src/components/Admin/CreateProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import api from '../Api/api.jsx';
+import api from '../Api/api.jsx'; 
 
 function CreateProduct() {
     const [productDeets, setProductDeets] = useState({
@@ -8,7 +8,7 @@ function CreateProduct() {
         name: '',
         unit_price: '',
         date_bought: '',
-        status: ''
+        status: 'Available'
     });
 
     const handleInputChange = (e) => {
@@ -31,7 +31,7 @@ function CreateProduct() {
                     name: '',
                     unit_price: '',
                     date_bought: '',
-                    status: ''
+                    status: 'Available'
                 });
             } else {
                 console.log("Could not create product.");
@@ -43,31 +43,37 @@ function CreateProduct() {
 
     return (
         <div className='sect-container mt-[20px]'>
-            <h1 className='comp-title text-hover-blue font-bold capitalize text-3xl mb-[15px]'> Create Products</h1>
+            <h1 className='comp-title text-hover-blue font-bold capitalize text-3xl mb-[15px]'>Create Products</h1>
             <div className="form-container">
                 <form className="product-form" onSubmit={handleSubmit}>
-                    <label htmlFor="serial_number">Serial Number</label>
-                    <input type="text" placeholder='serial_number' name='serial_number' value={productDeets.serial_number} onChange={handleInputChange} required />
-        
-                    <label htmlFor="category">Category</label>
-                    <input type="text" placeholder='category' name='category' value={productDeets.category} onChange={handleInputChange} required />
-
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder='name' name='name' value={productDeets.name} onChange={handleInputChange} required />
-
-                    <label htmlFor="unit_price">Unit Price</label>
-                    <input type="text" placeholder='unit_price' name='unit_price' value={productDeets.unit_price} onChange={handleInputChange} required />
-        
-                    <label htmlFor="date_bought">Date Bought</label>
-                    <input type="date" placeholder='date' name='date_bought' value={productDeets.date_bought} onChange={handleInputChange} required />
-        
-                    <label htmlFor="status">Status</label>
-                    <select name="status" value={productDeets.status} onChange={handleInputChange}>
-                        <option value="Available">Available</option>
-                        <option value="Assigned">Assigned</option>
-                    </select>
-        
-                    <button type="submit">Submit</button>
+                    <div className="form-group">
+                        <label htmlFor="serial_number" className='prod-label'>Serial Number</label>
+                        <input type="text" placeholder='Serial Number' name='serial_number' value={productDeets.serial_number} onChange={handleInputChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="category" className='prod-label'>Category</label>
+                        <input type="text" placeholder='Category' name='category' value={productDeets.category} onChange={handleInputChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="name" className='prod-label'>Name</label>
+                        <input type="text" placeholder='Name' name='name' value={productDeets.name} onChange={handleInputChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="unit_price" className='prod-label'>Unit Price</label>
+                        <input type="text" placeholder='Unit Price' name='unit_price' value={productDeets.unit_price} onChange={handleInputChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="date_bought" className='prod-label'>Date Bought</label>
+                        <input type="date" name='date_bought' value={productDeets.date_bought} onChange={handleInputChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="status" className='prod-label'>Status</label>
+                        <select name="status" value={productDeets.status} onChange={handleInputChange}>
+                            <option value="Available">Available</option>
+                            <option value="Assigned">Assigned</option>
+                        </select>
+                    </div>
+                    <button type="submit" className='btn'>Submit</button>
                 </form>
             </div>
         </div>
