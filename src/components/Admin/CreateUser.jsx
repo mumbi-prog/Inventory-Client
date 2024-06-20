c0c3d1b73315fbc17619a77ccb5fcc311c79de30
mumbi <mumbi099@gmail.com>
2024-06-20 11:44:52 +0300

    Updated Create User model to select dept from existing categories

--- src/components/Admin/CreateUser.jsx
+++ src/components/Admin/CreateUser.jsx
@@ -18,6 +18,16 @@ function CreateUser() {
         });
     };
 
+     const CATEGORIES= ['Stores', 'IT', 'Maintenance', 'Operations', 'Drivers', 'Finance'];
+
+    const handleCategoryChange = (e) => {
+        const {name, value} = e.target;
+        setUserDeets({
+            ...userDeets,
+            [name]: value
+        });
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault(); 
         try {
@@ -62,12 +72,20 @@ function CreateUser() {
                         <input className='input mt-3 p-2 rounded-md bg-gray-bkg focus:outline-none'
                             type="text" placeholder='Email' name="email" value={userDeets.email} onChange={handleInputChange} required
                         />
-                    </div>
-                    <div className="form-group mb2 mb-[10px]">
-                        <label htmlFor="department" className='label text-sm font-medium text-gray-700'>Department</label>
-                        <input className='input mt-3 p-2 rounded-md bg-gray-bkg focus:outline-none'
-                            type="text" placeholder='Department' name="department" value={userDeets.department} onChange={handleInputChange} required
-                        />
+                    </div> 
+                     <div className="form-group mb2 mb-[10px]">
+                        <label htmlFor="department" className='label text-sm font-medium text-gray-700'> Department:</label>
+                            <select
+                                    name="category" value={userDeets.department} onChange={handleCategoryChange}
+                                    className="input mt-3 p-2 rounded-md bg-gray-bkg focus:outline-none" required
+                                    >
+                                        <option value="">Select department</option>
+                                        {CATEGORIES.map((category) => (
+                                            <option key={category} value={category}>
+                                                {category}
+                                            </option>
+                                        ))}
+                            </select>
                     </div>
                     
                     <button type="submit" className='btn'>Create User</button>
