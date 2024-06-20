93c5e96ca5a0774ba551e36c51f6d92e0dd14f5c
mumbi <mumbi099@gmail.com>
2024-06-20 10:52:53 +0300

    Added useState for data and handleInputChange

--- src/components/Admin/UpdateUserModal.jsx
+++ src/components/Admin/UpdateUserModal.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function UpdateUserModal() {
+function UpdateUserModal({onClose, onUpdate, userData}) {
+    const[formData, setFormData] = useState(userData);
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
   return (
     <div>UpdateUserModal</div>
   )
