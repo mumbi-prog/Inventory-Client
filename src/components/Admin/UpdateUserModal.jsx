ba0162cbc763f3e624a3fc43cfe3674c41c8f0fc
mumbi <mumbi099@gmail.com>
2024-06-20 13:04:32 +0300

    update on updateusermodel

--- src/components/Admin/UpdateUserModal.jsx
+++ src/components/Admin/UpdateUserModal.jsx
@@ -27,7 +27,38 @@ function UpdateUserModal({onClose, onUpdate, userData}) {
     }
   return (
     <div>
-        
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="first_name">First Name</label>
+                    <input type="text" name='first_name' placeholder='First Name' value={formData.first_name} onChange={handleInputChange} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="last_name">First Name</label>
+                    <input type="text" name='last_name' placeholder='Last Name' value={formData.last_name} onChange={handleInputChange} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">First Name</label>
+                    <input type="text" name='first_name' placeholder='First Name' value={formData.email} onChange={handleInputChange} required/>
+                </div>
+                 <div className="form-group mb2 mb-[10px]">
+                        <label htmlFor="department" className='label text-sm font-medium text-gray-700'> Department:</label>
+                            <select
+                                    name="category" value={formData.department} onChange={handleCategoryChange}
+                                    className="input mt-3 p-2 rounded-md bg-gray-bkg focus:outline-none" required
+                                    >
+                                        <option value="">Select department</option>
+                                        {CATEGORIES.map((category) => (
+                                            <option key={category} value={category}>
+                                                {category}
+                                            </option>
+                                        ))}
+                            </select>
+                </div>
+                
+
+            </form>
+        </div>
     </div>
   )
 }
