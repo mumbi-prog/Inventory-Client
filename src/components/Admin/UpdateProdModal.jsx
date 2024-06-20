8673da89c518d68b043e302c83f1b3ee65cb9070
mumbi <mumbi099@gmail.com>
2024-06-20 11:00:31 +0300

    Added function to handle InputChange

--- src/components/Admin/UpdateProdModal.jsx
+++ src/components/Admin/UpdateProdModal.jsx
@@ -3,7 +3,13 @@ import React, { useState } from 'react'
 function UpdateProdModal({onClose, onUpdate, prodData}) {
     const [formData, setFormData] = useState(prodData);
 
-    
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
 
   return (
     <div>UpdateProdModal</div>
