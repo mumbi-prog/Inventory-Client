121947a33b273ba07ae86d2ad6251ec81890b359
mumbi <mumbi099@gmail.com>
2024-06-22 12:37:02 +0300

    Updated the update modal

--- src/components/Admin/UpdateUserModal.jsx
+++ src/components/Admin/UpdateUserModal.jsx
@@ -28,25 +28,25 @@ function UpdateUserModal({onClose, onUpdate, userData}) {
     }
   return (
     <div className='modal-overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50'>
-        <div className='modal-content bg-white border border-gray-300 rounded-md p-5 w-[400px] shadow-md text-left'>
-            <form onSubmit={handleSubmit}>
+        <div className='modal-content bg-white border border-gray-300 rounded-md p-5 w-[450px] shadow-md text-left'>
+            <form onSubmit={handleSubmit} className="user-details block">
                 <div className="form-group">
-                    <label htmlFor="first_name">First Name</label>
+                    <label htmlFor="first_name" className='label text-sm font-medium text-gray-700'>First Name</label>
                     <input type="text" name='first_name' placeholder='First Name' value={formData.first_name} onChange={handleInputChange} required/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="last_name">Last Name</label>
-                    <input type="text" name='last_name' placeholder='Last Name' value={formData.last_name} onChange={handleInputChange} required/>
+                    <label htmlFor="last_name" className='label text-sm font-medium text-gray-700'>Last Name</label>
+                    <input  type="text" name='last_name' placeholder='Last Name' value={formData.last_name} onChange={handleInputChange} required/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name='remail' placeholder='Email' value={formData.email} onChange={handleInputChange} required/>
+                    <label htmlFor="email" className='label text-sm font-medium text-gray-700'>Email</label>
+                    <input type="text" name='email' placeholder='Email' value={formData.email} onChange={handleInputChange} required/>
                 </div>
-                 <div className="form-group mb2 mb-[10px]">
+                 <div className="form-group mb-[20px]">
                         <label htmlFor="department" className='label text-sm font-medium text-gray-700'> Department:</label>
                            <select
                                 name="department" value={formData.department} onChange={handleCategoryChange}
-                                className="input mt-3 p-2 rounded-md bg-gray-bkg focus:outline-none" required
+                                className="input mt-2 p-2 rounded-md bg-gray-bkg focus:outline-none" required
                             >
                                 <option value="">Select department</option>
                                 {CATEGORIES.map((category) => (
@@ -60,8 +60,8 @@ function UpdateUserModal({onClose, onUpdate, userData}) {
                 </div>
                 
                 <div className="button-container">
-                    <button type='submit'>Update</button>
-                    <button onClick={onClose}>Cancel</button>
+                    <button type='submit' className="update-btn  border-white-500 text-black-600 transparent px-4 py-2 rounded-full mr-4 hover:bg-red-600">Update</button>
+                    <button onClick={onClose} className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400">Cancel</button>
                 </div>
             
             </form>
