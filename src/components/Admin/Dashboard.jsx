b860efffc2eb4c648e29a3b65d8194f66d60018f
mumbi <mumbi099@gmail.com>
2024-06-24 12:31:55 +0300

    Added search feature for products

--- src/components/Admin/Dashboard.jsx
+++ src/components/Admin/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import WelcomeAdmin from './WelcomeAdmin';
 import api from '../Api/api';
 import './comp-specific.css';
 
@@ -9,6 +8,7 @@ function Dashboard() {
   const [totalProducts, setTotalProducts] = useState(0);
   const [totalAssignedProducts, setTotalAssignedProducts] = useState(0);
   const [totalUsers, setTotalUsers] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,32 +56,46 @@ function Dashboard() {
     }
   };
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter(product =>
+    product.serial_number.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.category.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className='sect-container mt-[20px]'>
-      {/* <WelcomeAdmin /> */}
       <h1 className='comp-title text-hover-blue font-bold capitalize text-3xl mb-[15px]'>Dashboard</h1>
 
-      <div className="dashcards-container ">
-        <div className="prod-card bg-rose-600 flex flex-col justify-center justify-around items-center text-center py-[20px] w-[230px] hover:bg-rose-700">
+      <div className="dashcards-container">
+        <div className="prod-card bg-rose-600 flex flex-col justify-center justify-around items-center text-center py-[10px] w-[230px] hover:bg-rose-700">
           <h3>Total Products</h3>
           <p>{totalProducts}</p>
         </div>
-        <div className="assi-card bg-fuchsia-600 flex flex-col justify-center justify-around items-center text-center py-[20px] w-[230px] hover:bg-fuchsia-700">
+        <div className="assi-card bg-fuchsia-600 flex flex-col justify-center justify-around items-center text-center py-[10px] w-[230px] hover:bg-fuchsia-700">
           <h3>Total Assigned Products</h3>
           <p>{totalAssignedProducts}</p>
         </div>
-        <div className="users-card bg-emerald-600 flex flex-col justify-center justify-around items-center text-center py-[20px] w-[230px] hover:bg-emerald-700">
+        <div className="users-card bg-emerald-600 flex flex-col justify-center justify-around items-center text-center py-[10px] w-[230px] hover:bg-emerald-700">
           <h3>Total Users</h3>
           <p>{totalUsers}</p>
         </div>
       </div>
 
-      <div className="products-table-container rounded-md bg-gray-100 py-[20px] px-[20px] inline-block mt-[25px]">
+    <div className="search-container p-[5px] mt-[10px] mb-[5px] h-[35px] w-[300px] flex items-center justify-center">
+        <input type="text" className="search-feature border border-black rounded-md border-none outline-none py-[1px] px-[5px] font-light font-playfair text-sm bg-transparent"  
+        placeholder="Search by serial number or category" value={searchTerm} onChange={handleSearchChange}/>
+    </div> 
+
+    <div className="products-table-container rounded-md bg-gray-100 py-[20px] px-[20px] inline-block mt-[15px]">
+      
         <div className="products-table">
           <table className="prod-container">
             <thead>
               <tr>
-                <th className="w-[180px] py-[-5px] px-[0px] bg-gray-300 text-black font-medium text-center text-sm border-r-[20px]"> S_Number </th>
+                <th className="w-[180px] py-[-5px] px-[0px] bg-gray-300 text-black font-medium text-center text-sm border-r-[20px]"> Serial Number </th>
                 <th className="w-[170px] py-[-5px] px-[15px] bg-gray-300 text-black font-medium text-center text-sm border-r-[20px]"> Category </th>
                 <th className="w-[170px] py-[-5px] px-[45px] bg-gray-300 text-black font-medium text-center text-sm border-r-[20px]"> Name </th>
                 <th className="w-[50px] py-[-5px] px-[25px] bg-gray-300 text-black font-medium text-center text-sm border-r-[20px]"> Price </th>
@@ -91,7 +105,7 @@ function Dashboard() {
               </tr>
             </thead>
             <tbody>
-              {products.map((product, index) => (
+              {filteredProducts.map((product, index) => (
                 <tr key={index} className='row-detail border-l-4 border-transparent hover:border-blue-500 p-[20px] text-left hover:bg-blue-100 text-sm'>
                   <td className='py-[10px] px-[10px]'>{product.serial_number}</td>
                   <td className='py-[10px] px-[25px]'>{product.category}</td>
