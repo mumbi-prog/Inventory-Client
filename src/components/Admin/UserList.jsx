ae2e990ab4c5e112dab8233a6d095859ad719da5
mumbi <mumbi099@gmail.com>
2024-06-26 12:29:16 +0300

    Implemented search functionality to filter users by name or email.

--- src/components/Admin/UserList.jsx
+++ src/components/Admin/UserList.jsx
@@ -8,23 +8,21 @@ import { RiDeleteBinLine } from "react-icons/ri";
 
 function UserList() {
     const [userDetails, setUserDetails] = useState([]);
-
     const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
     const [userToUpdate, setUserToUpdate] = useState(null);
-
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
     const [userToDelete, setUserToDelete] = useState(null);
-
-    // search beginning
     const [searchTerm, setSearchTerm] = useState('');
 
-    const handleSearchChange = (user) => {
-    setSearchTerm(user.target.value);
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
     };
 
-   
-    // search end
-    
+    const filteredUsers = userDetails.filter(user =>
+        user.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     useEffect(() => {
         const fetchUserList = async () => {
@@ -53,7 +51,7 @@ function UserList() {
         setUserToUpdate(null);
     };
 
-     const openDeleteModal = (user) => {
+    const openDeleteModal = (user) => {
         setIsDeleteModalOpen(true);
         setUserToDelete(user);
     };
@@ -64,58 +62,60 @@ function UserList() {
     };
 
     const handleUpdateUser = (updatedUserData) => {
-    api
-        .patch(`/users/${userToUpdate.id}`, updatedUserData)
-        .then((response) => {
-            if (response.status === 200) {
-                api.get(`http://localhost:3000/users/${userToUpdate.id}`)
-                    .then((response) => {
-                        if (response.status === 200) {
-                            setUserDetails((prevUsers) =>
-                                prevUsers.map((user) =>
-                                    user.id === userToUpdate.id ? { ...user, ...response.data } : user
-                                )
-                            );
-                            closeUpdateModal();
-                        } else {
-                            console.error('Failed to fetch updated user details');
-                        }
-                    })
-                    .catch((error) => {
-                        console.error('Error fetching updated user details:', error);
-                    });
-            } else {
-                console.error('Failed to update user');
-            }
-        })
-        .catch((error) => {
-            console.error('Error updating user:', error);
-        });
+        api.patch(`/users/${userToUpdate.id}`, updatedUserData)
+            .then((response) => {
+                if (response.status === 200) {
+                    api.get(`http://localhost:3000/users/${userToUpdate.id}`)
+                        .then((response) => {
+                            if (response.status === 200) {
+                                setUserDetails((prevUsers) =>
+                                    prevUsers.map((user) =>
+                                        user.id === userToUpdate.id ? { ...user, ...response.data } : user
+                                    )
+                                );
+                                closeUpdateModal();
+                            } else {
+                                console.error('Failed to fetch updated user details');
+                            }
+                        })
+                        .catch((error) => {
+                            console.error('Error fetching updated user details:', error);
+                        });
+                } else {
+                    console.error('Failed to update user');
+                }
+            })
+            .catch((error) => {
+                console.error('Error updating user:', error);
+            });
     };
 
     const confirmDeleteUser = (user) => {
-    closeDeleteModal();
-
-    api
-      .delete(`/users/${user.id}`)
-      .then((response) => {
-        if (response.status === 204) {
-          setUserDetails((prevUsers) => prevUsers.filter((e) => e.id !== user.id));
-        } else {
-          console.error('Failed to delete user');
-        }
-      })
-      .catch((err) => {
-        console.error('Error while deleting user:', err);
-      });
-  };
-
+        closeDeleteModal();
+        api.delete(`/users/${user.id}`)
+            .then((response) => {
+                if (response.status === 204) {
+                    setUserDetails((prevUsers) => prevUsers.filter((e) => e.id !== user.id));
+                } else {
+                    console.error('Failed to delete user');
+                }
+            })
+            .catch((err) => {
+                console.error('Error while deleting user:', err);
+            });
+    };
 
     return (
         <div className='sect-container mt-[20px]'>
             <h1 className='comp-title text-hover-blue font-bold capitalize text-3xl mb-[15px]'>All Users</h1>
+            <div className="search-container p-[5px] mt-[10px] mb-[5px] h-[35px] w-[300px] flex items-center justify-center">
+                        <input
+                            type="text" className="search-feature border border-black rounded-md border-none outline-none py-[1px] px-[5px] font-light font-playfair text-sm bg-transparent"
+                            placeholder="Search by name or email" value={searchTerm} onChange={handleSearchChange} />
+            </div> 
             <div className="users-table-container rounded-md bg-gray-100 py-[20px] px-[50px] inline-block mt-[25px]">
                 <div className="users-table flex">
+                    
                     <table>
                         <thead className='bg-hover-blue'>
                             <tr>
@@ -123,14 +123,12 @@ function UserList() {
                                 <th className="w-[200px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px] text-sm">Email</th>
                                 <th className="w-[180px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center border-r-[20px] text-sm">Department</th>
                                 <th className="w-[50px] py-[10px] px-[20px] bg-gray-300 text-black font-medium text-center text-sm"></th>
-                            
                             </tr>
                         </thead>
                         <tbody>
-                            {userDetails.map((user, index) => (
-                                <tr key={index} className='row-detail border-l-4 border-transparent hover:border-blue-500 p-[20px] text-left hover:bg-blue-100 text-sm'>
+                            {filteredUsers.map((user) => (
+                                <tr key={user.id} className='row-detail border-l-4 border-transparent hover:border-blue-500 p-[20px] text-left hover:bg-blue-100 text-sm'>
                                     <td className='py-[10px] px-[20px]'>{user.first_name} {user.last_name}</td>
-                                    {/* <td className='py-[10px] px-[30px]'>{user.last_name}</td> */}
                                     <td className='py-[10px] px-[30px]'>{user.email}</td>
                                     <td className='py-[10px] px-[30px]'>{user.department}</td>
                                     <td className='py-[10px] px-[30px]'>
@@ -138,9 +136,7 @@ function UserList() {
                                             <CiEdit onClick={() => openUpdateModal(user)} className="cursor-pointer" title='Update User'/>
                                             <RiDeleteBinLine onClick={() => openDeleteModal(user)} className='cursor-pointer' title='Delete User'/>
                                         </div>
-
                                     </td>
-
                                 </tr>
                             ))}
                         </tbody>
@@ -153,7 +149,6 @@ function UserList() {
             {isDeleteModalOpen && (
                 <DeleteConfirmationModal onCancel={closeDeleteModal} onConfirm={() => confirmDeleteUser(userToDelete)} userData={userToDelete}/>
             )}
-
         </div>
     );
 }
