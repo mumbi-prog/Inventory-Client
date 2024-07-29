276081da541a3a49f94c2792c3c07ec2e9ee14bc
mumbi <mumbi099@gmail.com>
2024-07-29 12:03:55 +0300

    Update on Auth-comps to only allow autorized users

--- src/components/Logins/AdminLogin.jsx
+++ src/components/Logins/AdminLogin.jsx
@@ -3,6 +3,7 @@ import api from '../Api/api.jsx';
 import { useNavigate, Link } from 'react-router-dom';
 import './logins.css';
 import { PiEyeClosed, PiEyeBold } from 'react-icons/pi';
+import ErrorModal from '../Admin/ErrorModal.jsx';
 
 function AdminLogin() {
   const [email, setEmail] = useState('');
@@ -15,11 +16,20 @@ function AdminLogin() {
     e.preventDefault();
 
     try {
-      await api.post('http://localhost:3000/login', { email, password });
+      await api.post('http://localhost:3000/login', {
+        email,
+        password
+      });
+
       console.log('Login successful');
       navigate('/main');
     } catch (error) {
-      setError('Invalid email or password');
+      const errorMessage = error.response.data.error;
+      if (errorMessage === 'Unauthorized! Unauthorized!') {
+        setError(errorMessage);
+      } else {
+        setError('Unauthorized to log in. Please use an authorized email address.');
+      }
       console.error('Login error:', error);
     }
   };
@@ -28,44 +38,36 @@ function AdminLogin() {
     <div className='auth-container bg-gray-bkg h-screen flex justify-center items-center'>
       <div className='auth-wrapper ml-[100px] flex w-[1000px]'>
         <div className="login-bkg w-[50%]">
-          <h3 className='font-playfair text-6xl font-bold text-white text-center py-[60%]'>Welcome Back</h3>
+          <h3 className='font-playfair text-6xl font-bold text-white text-center py-[55%]'>Welcome <br /> Back</h3>
         </div>
-        <div className='form-content bg-white w-[50%] px-[3%]'>
-          <h5 className="text-4xl font-bold mt-[20px] mb-[5px] text-black"> Log In </h5>
-          <p className="text-black mb-[20px]">Not a member? <Link to="/signup" className='underline text-main-blue font-bold'>Sign up</Link> instead</p>
+        <div className='form-content bg-white w-[50%]  px-[3%]'>
+          <h5 className="text-4xl font-bold mt-[60px] mb-[20px] text-black"> Log In </h5>
+          <p className="text-black mb-[50px]">Not a member? <Link to="/signup" className='underline text-main-blue font-bold'>Sign Up</Link> instead</p>
 
           <div className="form">
             <form onSubmit={handleSubmit}>
-              <div className="mb-2">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+              <div className="mb2 mb-[20px]">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Email </label>
                 <input
-                  placeholder="email@dakawou.com"
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="mt-1 p-2 w-full rounded-md bg-gray-bkg focus:outline-none"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="email@dakawou.com" type="email" id="email" name="email"
+                  className="mt-3 p-2 w-full rounded-md bg-gray-bkg focus:outline-none" required
+                  value={email} onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div className="mb-[10px]">
+              <div className="mb-2 password-field">
                 <PasswordInput
-                  name="password"
-                  title="Password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  showPassword={showPassword}
+                  name="password" title="Password" placeholder="Enter Password"
+                  value={password} onChange={(e) => setPassword(e.target.value)} showPassword={showPassword}
                   onTogglePassword={() => setShowPassword(!showPassword)}
                 />
               </div>
+
               <div className="auth-button flex justify-center items-center">
-                <button className='login uppercase bg-other-blue text-white font-semibold px-4 py-2 rounded-md hover:bg-hover-blue w-full mt-1'>
-                  Log In
+                <button className='account-login uppercase bg-other-blue text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-800 w-full mt-7'>
+                  Login
                 </button>
               </div>
-              {error && <p className="text-red-500 mt-2">{error}</p>}
+              {error && <ErrorModal errorMessage={error} onClose={() => setError('')} />}
             </form>
           </div>
         </div>
