abe35065cd6befe00b703915b884a5210735bd33
mumbi <mumbi099@gmail.com>
2024-08-23 12:36:50 +0300

    Import NotificationCard

--- src/components/Admin/UpdateProdModal.jsx
+++ src/components/Admin/UpdateProdModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from '../Api/api';
 import './comp-specific.css';
+import NotificationCard from './NotificationCard';
 
 function UpdateProdModal({ onClose, onUpdate, prodData }) {
   const [formData, setFormData] = useState(prodData);
