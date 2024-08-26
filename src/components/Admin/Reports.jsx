87a5aae1b4cde16c6771d8f824095aa021e36baf
mumbi <mumbi099@gmail.com>
2024-08-26 15:15:02 +0300

    Commented out reports content

--- src/components/Admin/Reports.jsx
+++ src/components/Admin/Reports.jsx
@@ -1,27 +1,27 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
 
-function Reports() {
-  const [userProds, setUserProds] = useState('');
+// function Reports() {
+//   const [userProds, setUserProds] = useState('');
 
-  useEffect(() => {
-    const fetchUserProds = async () => {
-      try{
-      }
-      catch(err){
-        console.error("Could not fetch User Products", err)
-      }
-    }
-  })
+//   useEffect(() => {
+//     const fetchUserProds = async () => {
+//       try{
+//       }
+//       catch(err){
+//         console.error("Could not fetch User Products", err)
+//       }
+//     }
+//   })
 
-  return (
-    <div>
-      <div className="user-report">
-        <h1>User's Report</h1>
-        <p>Name</p>
+//   return (
+//     <div>
+//       <div className="user-report">
+//         <h1>User's Report</h1>
+//         <p>Name</p>
 
-      </div>
-    </div>
-  )
-}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Reports
+// export default Reports
