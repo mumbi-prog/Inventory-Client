4fc1b7cfe64341aeac9f278e1df8405945eb7c03
mumbi <mumbi099@gmail.com>
2024-09-11 11:27:03 +0300

    api.jsx update

--- src/components/Api/api.jsx
+++ src/components/Api/api.jsx
@@ -1,8 +1,25 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'https://inventory-api-77ys.onrender.com', 
+//   withCredentials: true,
+// });
+
+// export default api;
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://inventory-api-77ys.onrender.com', 
+  baseURL: 'https://inventory-api-77ys.onrender.com',
   withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  if (csrfToken) {
+    config.headers['X-CSRF-Token'] = csrfToken;
+  }
+  return config;
 });
 
 export default api;
