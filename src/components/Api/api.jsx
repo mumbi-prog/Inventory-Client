25da519112df15941aef0d6bd874a655e0fd4b14
mumbi <mumbi099@gmail.com>
2024-09-11 11:51:44 +0300

    Revert

--- src/components/Api/api.jsx
+++ src/components/Api/api.jsx
@@ -1,25 +1,8 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'https://inventory-api-77ys.onrender.com', 
-//   withCredentials: true,
-// });
-
-// export default api;
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://inventory-api-77ys.onrender.com',
-  withCredentials: true, 
+  baseURL: 'https://inventory-api-77ys.onrender.com', 
+  withCredentials: true,
 });
-
-
-api.interceptors.response.use(
-  response => response,
-  error => {
-    console.error('API Error:', error.response?.data || error.message);
-    return Promise.reject(error);
-  }
-);
 
 export default api;
